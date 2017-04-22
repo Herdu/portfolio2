29cb3d416e4db7f02a5c4463ccc843265ac32fb4
herdu <mateusz.dobrowolski71@gmail.com>
2017-04-22 20:42:51 +0200

    scrolling mechanism

--- script/main.js
+++ script/main.js
@@ -1,11 +1,10 @@
 /**
  * Created by matt on 19.04.17.
  */
-
+//**********************************/
+//*** MENU SECTION *************/
 
 var isMenu = false;
-
-
 
 var hideMenu = function(){
     $('header>section>i').toggleClass("rotate180");
@@ -38,7 +37,7 @@ var hideMenu = function(){
         },200);
 
     }
-}
+};
 
 
 $('.fa').on("click", hideMenu);
@@ -48,4 +47,100 @@ $('.menu-hidden').on("click", function(){
         hideMenu();
 });
 
+
 $('.menu-container > p').on("click", hideMenu);
+
+//**********************************/
+//*** SCROLL SECTION *************/
+
+
+
+var isScrolling = false;
+
+var myScrollEvent =  function(up){
+    if (isScrolling) return;
+    console.log("scroll event!");
+
+
+    var scroll = $(window).scrollTop();
+    var hasClosest = false;
+    var closest = 0;
+    var speed = 2000;
+    var margin = 100;
+
+
+    $.each($('body > section'),function(id){
+        var offset  = $(this).offset().top;
+
+        if (offset >= (scroll - margin) && !hasClosest){
+            closest = id;
+            hasClosest = true;
+            return;
+        }
+
+    });
+
+    if (!hasClosest)
+        closest = $('body > section').length-1;
+
+    var closestUp = closest -1;
+    if (closestUp < 0)
+        closestUp = 0;
+
+    isScrolling = true;
+
+
+    if (up)
+    {
+        $('html, body').animate({
+            scrollTop: $("body>section").eq(closestUp).offset().top + margin
+        }, speed, function() {
+            isScrolling = false;
+        });
+    }
+    else
+    {
+        $('html, body').animate({
+            scrollTop: $("body>section").eq(closest).offset().top + margin
+        }, speed, function() {
+            isScrolling = false;
+        });
+    }
+
+
+};
+
+
+
+
+var oldPosition = $(window).scrollTop();
+
+$(window).scroll(function(){
+
+    var newPosition = $(window).scrollTop();
+
+    if (!isScrolling){
+        if (oldPosition - newPosition >0){
+            myScrollEvent(true);
+        }
+        else{
+            myScrollEvent(false);
+        }
+    }
+
+
+
+    oldPosition = newPosition;
+});
+
+
+$('.scrollDown').on("click", function(){
+    console.log("click");
+    isScrolling = true;
+    $('html, body').animate({
+        scrollTop: $("#costam").offset().top
+    }, 300, function(){
+        isScrolling = false;
+    });
+})
+//**********************************/
